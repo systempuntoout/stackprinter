5d15a0c6b812f3c5ebb57b5c30cf108b2632a380
systempuntoout <michele.trimarchi@gmail.com>
2010-10-14 00:02:07 +0200

    transaction on topvoted

--- app/db/question.py
+++ app/db/question.py
@@ -13,15 +13,15 @@ class PrintedQuestionModel(db.Model):
         return "http://%s.com/questions/%d" % (self.service, self.question_id)
 
 def store(question_id, service, title, tags):
-    #Transaction would be an overkill
-    entity = PrintedQuestionModel.get_by_key_name(key_names = '%s_%s' % (question_id, service ) )
-    if entity:
-        entity.counter = entity.counter + 1
-        entity.put()
-    else:
-        PrintedQuestionModel(key_name = '%s_%s' % (question_id, service ), question_id = question_id,\
-                             service = service, title = title, tags = tags, counter = 1).put()
-
+    def _store_TX():
+        entity = PrintedQuestionModel.get_by_key_name(key_names = '%s_%s' % (question_id, service ) )
+        if entity:
+            entity.counter = entity.counter + 1
+            entity.put()
+        else:
+            PrintedQuestionModel(key_name = '%s_%s' % (question_id, service ), question_id = question_id,\
+                                 service = service, title = title, tags = tags, counter = 1).put()
+    db.run_in_transaction(_store_TX)
 def get_top_printed():
     query = PrintedQuestionModel.all().order('-counter')
     return query.fetch(RANKING_SIZE)
